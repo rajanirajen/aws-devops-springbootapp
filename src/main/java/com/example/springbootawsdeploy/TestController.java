05a2a7d47bc2d56f3d254e7d4c09329f48ff56fa
rajanirajen <88222668+rajanirajen@users.noreply.github.com>
2025-05-08 09:44:16 +0530

    Update TestController.java

--- src/main/java/com/example/springbootawsdeploy/TestController.java
+++ src/main/java/com/example/springbootawsdeploy/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/demo")
+@RequestMapping("/demos")
 public class TestController {
     @GetMapping("/data")
     public String getData() {
